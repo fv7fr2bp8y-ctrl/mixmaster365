5cf906c14c02cae0fa4160d9c886e6f2d5e96491
Spas Spasov <spasspasov@192.168.100.10>
2026-07-06 20:45:08 +0300

    Add free-from recipes app and breakfast fixes

---
+++ free-from/sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = "free-from-365-v1";
+const ASSETS = [
+  "/free-from/",
+  "/free-from/index.html",
+  "/free-from/manifest.json",
+  "/free-from/icon-192.png",
+  "/free-from/icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).catch(() => {}));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request).catch(() => caches.match("/free-from/index.html")))
+  );
+});
