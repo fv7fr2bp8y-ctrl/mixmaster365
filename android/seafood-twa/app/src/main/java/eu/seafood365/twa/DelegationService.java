c4e8fa5b4436546221d35cccbabfecd2f20fc16e
Spas Spasov <spasspasov@192.168.100.10>
2026-07-23 12:08:16 +0300

    Add Seafood Android release and rename Vegan app

---
+++ android/seafood-twa/app/src/main/java/eu/seafood365/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package eu.seafood365.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
